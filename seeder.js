633c83b5e091bcccb9803b99c8c9d4b47b704bb3
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-11 15:33:06 +0100

    seeder to import data from file

---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//load env variables
+dotenv.config({ path: './config/config.env' });
+
+//load models
+
+const Bootcamp = require('./models/Bootcamp');
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+//Read the json files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+//Imports into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('data imported ...');
+
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Delete the data from DB
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('data destroyed');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
