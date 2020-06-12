964c0095e61058d716241266142e014895b924ea
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-12 17:20:36 +0100

    delete course or update a course

--- Routes/courses.js
+++ Routes/courses.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const { getCourses, getCourse, addCourse } = require('../controlers/courses');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse
+} = require('../controlers/courses');
 
 const router = express.Router({ mergeParams: true }); //so the reroute works here
 
-router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/')
+  .get(getCourses)
+  .post(addCourse);
+
+router.route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse)
 
 module.exports = router;
