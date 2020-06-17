3fb25b20b5ff4718619f2f43d0acfc00139e1100
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-17 11:39:55 +0100

    set cookies

--- controlers/auth.js
+++ controlers/auth.js
@@ -16,12 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // create token
-  const token = user.getSignedJwtToken();
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendTokenResponse(user, 200, res);
 });
 
 //@desc      login user;
@@ -45,11 +40,25 @@ exports.login = asyncHandler(async (req, res, next) => {
   if (!isMatch) {
     return next(new ErrorResponse(`Invalid credentials`, 401));
   }
+  sendTokenResponse(user, 200, res);
+});
 
+//get token from model, create cookie and send response
+
+const sendTokenResponse = (user, statusCode, res) => {
   // create token
   const token = user.getSignedJwtToken();
-  res.status(200).json({
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ), //30 days from now time
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+  res.status(statusCode).cookie('token', token, options).json({
     success: true,
     token,
   });
-});
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -20,6 +21,9 @@ const app = express();
 
 //body parser
 app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
 
 //dev logging middleware
 if (process.env.NODE_ENV === 'development') {
