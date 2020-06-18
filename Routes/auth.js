bedf24bb7ec0f283399fa4128cf62c8a08a65f2d
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-18 16:21:47 +0100

    update details and password

--- Routes/auth.js
+++ Routes/auth.js
@@ -5,14 +5,19 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controlers/auth');
 const router = express.Router();
 const { protect } = require('../middleware/auth'); //to protect some routes
+const { update } = require('../models/User');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
