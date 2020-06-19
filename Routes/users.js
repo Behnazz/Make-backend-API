cd70f7a0658f68efd4021bf44ce792c26944bd9c
Behnaz Pourafshari <behnaz.pourafshari@storyshareplatform.com>
2020-06-19 11:13:17 +0100

    user CRUD

--- Routes/users.js
+++ Routes/users.js
@@ -11,7 +11,6 @@ const User = require('../models/User');
 
 const router = express.Router({ mergeParams: true }); //so the reroute works here
 
-
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth'); //to protect some routes
 
@@ -19,15 +18,8 @@ const { protect, authorize } = require('../middleware/auth'); //to protect some 
 router.use(protect);
 router.use(authorize('admin'));
 
-router
-  .route('/')
-  .get(advancedResults(User), getUsers)
-  .post(createUser)
+router.route('/').get(advancedResults(User), getUsers).post(createUser);
 
-router
-  .route('/:id')
-  .get(getUser)
-  .put(updateUser)
-  .delete(deleteUser);
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
 
-  module.exports = router
+module.exports = router;
